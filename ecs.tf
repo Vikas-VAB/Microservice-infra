5dfb114d822e6f09093032d83244d48a036ab6d4
Vikas-VAB <79959848+Vikas-VAB@users.noreply.github.com>
2021-09-01 11:09:46 +0530

    Update ecs.tf

--- ecs.tf
+++ ecs.tf
@@ -11,8 +11,8 @@ resource "aws_ecs_task_definition" "myapp-task" {
   memory = 2048
   container_definitions = jsonencode([
     {
-      name      = "sample"
-      image     = "vikasabdocker/jarvis:myapp"
+      name      = "usermanagement"
+      image     = "216012631294.dkr.ecr.us-east-1.amazonaws.com/microservices-1:latest"
       cpu       = 1
       memory    = 1024
       network_mode  = "awsvpc"
@@ -27,7 +27,50 @@ resource "aws_ecs_task_definition" "myapp-task" {
  }
 
 resource "aws_ecs_service" "myapp-svc" {
-  name            = "myapp-svc"
+  name            = "svc-notification"
+  cluster         = aws_ecs_cluster.myapp-cluster.id
+  task_definition = aws_ecs_task_definition.myapp-task.arn
+  desired_count   = 1
+  launch_type     = "FARGATE"
+  network_configuration {
+    security_groups  = [aws_security_group.myapp-sg.id]
+    subnets          = aws_subnet.myapp-subnet.*.id
+    assign_public_ip = true
+  }
+
+  load_balancer {
+    target_group_arn = aws_alb_target_group.myapp-tg.id
+    container_name   = "sample"
+    container_port   = 80
+  }
+}
+
+resource "aws_ecs_task_definition" "myapp-task{
+  family = "myapp-task"
+  execution_role_arn  = aws_iam_role.ecstaskexecutionrole123.arn
+  network_mode  = "awsvpc" 
+  requires_compatibilities = ["FARGATE"]
+  cpu = 1024
+  memory = 2048
+  container_definitions = jsonencode([
+    {
+      name      = "usermanagement"
+      image     = "216012631294.dkr.ecr.us-east-1.amazonaws.com/microservices-2:latest"
+      cpu       = 1
+      memory    = 1024
+      network_mode  = "awsvpc"
+      portMappings = [
+        {
+          containerPort = 80
+          hostPort      = 80
+        }
+      ]
+    }
+   ])
+ }
+
+resource "aws_ecs_service" "myapp-svc" {
+  name            = "svc-microservice"
   cluster         = aws_ecs_cluster.myapp-cluster.id
   task_definition = aws_ecs_task_definition.myapp-task.arn
   desired_count   = 1
